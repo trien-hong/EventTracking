3d909e6f01c41b5222755799cceb6db0d88d56d6
Trien_Hong <danielhong55@gmail.com>
2022-03-28 21:25:18 -0400

    database feature

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
+import os
 import flask
+from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
 
 app = flask.Flask(__name__)
+app.secret_key = os.getenv("APP_SECRET_KEY")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+if app.config["SQLALCHEMY_DATABASE_URI"].startswith("postgres://"):
+    app.config["SQLALCHEMY_DATABASE_URI"] = app.config[
+        "SQLALCHEMY_DATABASE_URI"
+    ].replace("postgres://", "postgresql://")
+
+db = SQLAlchemy(app, session_options={"autocommit": True})
 
 @app.route("/")
 def index():
@@ -11,5 +22,11 @@ def index():
 
 
 
-app.run()
+if __name__ == "__main__":
+    db.create_all()
+    from models import Users
 
+    app.run(
+        host=os.getenv("IP", "0.0.0.0"),
+        port=int(os.getenv("PORT", 8080)),
+    )
---
+++ models.py
@@ -0,0 +1,12 @@
+from flask_login import UserMixin
+from app import db
+
+
+class Users(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+
+    def __init__(self, email, password):
+        self.email = email
+        self.password = password
