bec5da6ad9324142bcc4e0c05e731470d166b9b5
Trien_Hong <danielhong55@gmail.com>
2022-03-31 01:54:59 -0400

    updated ticketmaster api & basic display

--- ticketmaster_api.py
+++ ticketmaster_api.py
@@ -4,16 +4,21 @@ from dotenv import find_dotenv, load_dotenv
 
 load_dotenv(find_dotenv())
 
-TICKERTMASTER_API_KEY = os.getenv("TICKETMASTER_API_KEY")
-zipcode = "90028"
+def getEvents(zipcode: str):
+    TICKERTMASTER_API_KEY = os.getenv("TICKETMASTER_API_KEY")
 
-url = "https://app.ticketmaster.com/discovery/v2/events?apikey=" + TICKERTMASTER_API_KEY + "&postalCode=" + zipcode + "&locale=*"
+    url = "https://app.ticketmaster.com/discovery/v2/events?apikey=" + TICKERTMASTER_API_KEY + "&postalCode=" + zipcode + "&locale=*"
 
-ticketmaster_request = requests.get(url=url)
+    ticketmaster_request = requests.get(url=url)
 
-ticketmaster_response_json = ticketmaster_request.json()
+    ticketmaster_response_json = ticketmaster_request.json()
 
-for x in ticketmaster_response_json["_embedded"]["events"]:
-    print(x["name"])
-    print(x["images"][3]["url"])
-    print(x["_embedded"]["venues"][0]["name"] + " " + x["_embedded"]["venues"][0]["address"]["line1"] + ", " + x["_embedded"]["venues"][0]["city"]["name"] + ", " + x["_embedded"]["venues"][0]["state"]["name"] + " " + x["_embedded"]["venues"][0]["postalCode"])
+    nameList = []
+    imageList = []
+    addressList = []
+
+    for x in ticketmaster_response_json["_embedded"]["events"]:
+        nameList.append(x["name"])
+        imageList.append(x["images"][3]["url"])
+        addressList.append(x["_embedded"]["venues"][0]["name"] + " " + x["_embedded"]["venues"][0]["address"]["line1"] + ", " + x["_embedded"]["venues"][0]["city"]["name"] + ", " + x["_embedded"]["venues"][0]["state"]["name"] + " " + x["_embedded"]["venues"][0]["postalCode"])
+    return zip(nameList, imageList, addressList)
