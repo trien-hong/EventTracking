0a1ebc90a91d30d6171d82ad079c8f9e87cfb991
aabdulhakim2 <aabdulhakim2@student.gsu.edu>
2022-04-08 09:11:23 -0400

    display the user events in profile

--- ticketmaster_api.py
+++ ticketmaster_api.py
@@ -4,25 +4,40 @@ from dotenv import find_dotenv, load_dotenv
 
 load_dotenv(find_dotenv())
 
-def getEvents(zipcode: str):
+def getEvents(parameter: str):
     TICKERTMASTER_API_KEY = os.getenv("TICKETMASTER_API_KEY")
 
-    url = "https://app.ticketmaster.com/discovery/v2/events?apikey=" + TICKERTMASTER_API_KEY + "&postalCode=" + zipcode + "&locale=*"
+    if len(parameter) == 5 and parameter.isnumeric():
+        url = "https://app.ticketmaster.com/discovery/v2/events?apikey=" + TICKERTMASTER_API_KEY + "&postalCode=" + parameter + "&locale=*"
 
-    ticketmaster_request = requests.get(url=url)
+        ticketmaster_request = requests.get(url=url)
 
-    ticketmaster_response_json = ticketmaster_request.json()
+        ticketmaster_response_json = ticketmaster_request.json()
 
-    idList = []
-    nameList = []
-    imageList = []
+        idList = []
+        nameList = []
+        imageList = []
 
-    for x in ticketmaster_response_json["_embedded"]["events"]:
-        idList.append(x["id"])
-        nameList.append(x["name"])
-        imageList.append(x["images"][0]["url"])
-    
-    return zip(idList, nameList, imageList)
+        for x in ticketmaster_response_json["_embedded"]["events"]:
+            idList.append(x["id"])
+            nameList.append(x["name"])
+            imageList.append(x["images"][0]["url"])
+        
+        return zip(idList, nameList, imageList)
+    else:
+        url = "https://app.ticketmaster.com/discovery/v2/events/" + parameter + "?apikey=" + TICKERTMASTER_API_KEY + "&locale=*"
+
+        ticketmaster_request = requests.get(url=url)
+
+        ticketmaster_response_json = ticketmaster_request.json()    
+
+        event = {
+            "id": ticketmaster_response_json["id"],
+            "name": ticketmaster_response_json["name"],
+            "eventImageURL": ticketmaster_response_json["images"][0]["url"]
+        }
+
+        return event
 
 def getEventDetails(eventId: str):
     TICKERTMASTER_API_KEY = os.getenv("TICKETMASTER_API_KEY")
@@ -31,20 +46,34 @@ def getEventDetails(eventId: str):
 
     ticketmaster_request = requests.get(url=url)
 
-    ticketmaster_response_json = ticketmaster_request.json()    
+    ticketmaster_response_json = ticketmaster_request.json()
 
-    eventDetails = {
-        "name": ticketmaster_response_json["name"],
-        "eventImageURL": ticketmaster_response_json["images"][0]["url"],
-        "startDate": ticketmaster_response_json["dates"]["start"]["localDate"],
-        "genre": ticketmaster_response_json["classifications"][0]["genre"]["name"],
-        "minPrice": ticketmaster_response_json["priceRanges"][0]["min"],
-        "maxPrice": ticketmaster_response_json["priceRanges"][0]["max"],
-        "venue": ticketmaster_response_json["_embedded"]["venues"][0]["name"],
-        "address": ticketmaster_response_json["_embedded"]["venues"][0]["address"]["line1"] + ", " + ticketmaster_response_json["_embedded"]["venues"][0]["city"]["name"] + ", " + ticketmaster_response_json["_embedded"]["venues"][0]["state"]["name"] + " " + ticketmaster_response_json["_embedded"]["venues"][0]["postalCode"],
-        "longitude": ticketmaster_response_json["_embedded"]["venues"][0]["location"]["longitude"],
-        "latitude": ticketmaster_response_json["_embedded"]["venues"][0]["location"]["latitude"]
-    }
+    if "priceRanges" in ticketmaster_response_json:
+        eventDetails = {
+            "name": ticketmaster_response_json["name"],
+            "eventImageURL": ticketmaster_response_json["images"][0]["url"],
+            "startDate": ticketmaster_response_json["dates"]["start"]["localDate"],
+            "genre": ticketmaster_response_json["classifications"][0]["genre"]["name"],
+            "minPrice": "$" + str(ticketmaster_response_json["priceRanges"][0]["min"]),
+            "maxPrice": "$" + str(ticketmaster_response_json["priceRanges"][0]["max"]),
+            "venue": ticketmaster_response_json["_embedded"]["venues"][0]["name"],
+            "address": ticketmaster_response_json["_embedded"]["venues"][0]["address"]["line1"] + ", " + ticketmaster_response_json["_embedded"]["venues"][0]["city"]["name"] + ", " + ticketmaster_response_json["_embedded"]["venues"][0]["state"]["name"] + " " + ticketmaster_response_json["_embedded"]["venues"][0]["postalCode"],
+            "longitude": ticketmaster_response_json["_embedded"]["venues"][0]["location"]["longitude"],
+            "latitude": ticketmaster_response_json["_embedded"]["venues"][0]["location"]["latitude"]
+        }
+    else:
+        eventDetails = {
+            "name": ticketmaster_response_json["name"],
+            "eventImageURL": ticketmaster_response_json["images"][0]["url"],
+            "startDate": ticketmaster_response_json["dates"]["start"]["localDate"],
+            "genre": ticketmaster_response_json["classifications"][0]["genre"]["name"],
+            "minPrice": "TBD",
+            "maxPrice": "TBD",
+            "venue": ticketmaster_response_json["_embedded"]["venues"][0]["name"],
+            "address": ticketmaster_response_json["_embedded"]["venues"][0]["address"]["line1"] + ", " + ticketmaster_response_json["_embedded"]["venues"][0]["city"]["name"] + ", " + ticketmaster_response_json["_embedded"]["venues"][0]["state"]["name"] + " " + ticketmaster_response_json["_embedded"]["venues"][0]["postalCode"],
+            "longitude": ticketmaster_response_json["_embedded"]["venues"][0]["location"]["longitude"],
+            "latitude": ticketmaster_response_json["_embedded"]["venues"][0]["location"]["latitude"]
+        }
 
     return eventDetails
 
