937fa325eba50b7e8c531d9ed6760ff55b3cfcb8
Trien Hong <Trien_Hong>
2022-12-17 06:14:57 -0500

    added onKeyPress for enter key in textfield

--- event_tracking_django_react/react_frontend/src/components/Header.js
+++ event_tracking_django_react/react_frontend/src/components/Header.js
@@ -1,13 +1,14 @@
 import { useState } from 'react';
 import { AppBar, Toolbar, Button, TextField } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
-import { Link } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 
 function Header() {
     const [buttonColorEvents, setButtonColorEvents] = useState("primary");
     const [buttonColorProfile, setButtonColorProfile] = useState("primary");
     const [buttonColorSearch, setButtonColorSearch] = useState("primary");
     const [search, setSearch] = useState("");
+    const navigate = useNavigate();
 
     function changeButtonColorEvents() {
         setButtonColorEvents("success");
@@ -27,12 +28,19 @@ function Header() {
         setButtonColorSearch("success");
     }
 
+    function goToSearch(event) {
+        if(event.key === "Enter") {
+            changeButtonColorSearch();
+            navigate(`/search/input/${search}/`);
+        }
+    }
+
     return (
         <div className="header">
             <AppBar position="static" sx={{ background: "gray" }}>
                 <Toolbar>
                     <Button sx={{ margin: 'auto' }} color={buttonColorEvents} variant="contained" component={Link} to="/events" onClick={() => { changeButtonColorEvents(); }}>EVENTS</Button>
-                    <TextField sx={{ background: "white", width: 375, mr: 1 }} label="Search events here..." variant="filled" onChange={(event) => { setSearch(event.target.value); }}/>
+                    <TextField sx={{ background: "white", width: 375, mr: 1 }} label="Search events here..." variant="filled" onChange={(event) => { setSearch(event.target.value); }} onKeyPress={(event) => { goToSearch(event); }}/>
                     <Button color={buttonColorSearch} variant="contained" component={Link} to={`/search/input/${search}/`} onClick={() => { changeButtonColorSearch(); }}>
                         <SearchIcon/>
                     </Button>
